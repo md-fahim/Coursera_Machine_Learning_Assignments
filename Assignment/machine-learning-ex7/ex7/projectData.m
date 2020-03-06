5eeefe5e0ed9576557f9849600551e06a40b6eac
md-fahim <fahimcse381@gmail.com>
2020-03-06 12:16:47 +0600

    Matlab/Octave

---
+++ Assignment/machine-learning-ex7/ex7/projectData.m
@@ -0,0 +1,32 @@
+function Z = projectData(X, U, K)
+%PROJECTDATA Computes the reduced data representation when projecting only 
+%on to the top k eigenvectors
+%   Z = projectData(X, U, K) computes the projection of 
+%   the normalized inputs X into the reduced dimensional space spanned by
+%   the first K columns of U. It returns the projected examples in Z.
+%
+
+% You need to return the following variables correctly.
+Z = zeros(size(X, 1), K);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the projection of the data using only the top K 
+%               eigenvectors in U (first K columns). 
+%               For the i-th example X(i,:), the projection on to the k-th 
+%               eigenvector is given as follows:
+%                    x = X(i, :)';
+%                    projection_k = x' * U(:, k);
+%
+
+U_reduce= U(:,1:K);
+
+for i = 1 : size(X,1)  % for every row of X(i) we need to project
+    x = X(i,:);
+    Z(i, :) = x * U_reduce; % because we have same row of z that has X has
+end
+
+
+
+% =============================================================
+
+end
